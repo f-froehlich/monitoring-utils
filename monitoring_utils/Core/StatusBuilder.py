b99ff53c86cc1ab9aafeeac6ec6a5fe94a09e62f
Fabian Fröhlich <mail@f-froehlich.de>
2020-12-12 18:12:58 +0100

    Send kill to child processes

--- monitoring_utils/Core/StatusBuilder.py
+++ monitoring_utils/Core/StatusBuilder.py
@@ -28,7 +28,10 @@
 
 
 import logging
+import os
 import sys
+
+import psutil
 
 
 class StatusBuilder:
@@ -56,6 +59,23 @@ class StatusBuilder:
         logging.debug('Add unknown message: "' + message + '"')
         self.__unknown.append(message)
 
+    def __exit(self, status_code):
+        kill = False
+        parent = psutil.Process(os.getpid())
+        for child in parent.children(recursive=True):
+            try:
+                self.__logger.info('Kill child processes with PID "{pid}"'.format(pid=child.pid))
+                child.kill()
+            except Exception as e:
+                self.__logger.info('Can\'t kill child processes with PID "{pid}"'.format(pid=child.pid))
+                kill = True
+                self.__logger.debug(e)
+        if kill:
+            self.__logger.info('Kill process')
+            parent.kill()
+        else:
+            sys.exit(status_code)
+
     def exit(self, all_outputs=False):
 
         exit_code = None
@@ -63,7 +83,7 @@ class StatusBuilder:
             for message in self.__critical:
                 print('CRITICAL: ' + message)
             if not all_outputs:
-                sys.exit(2)
+                self.__exit(2)
             else:
                 exit_code = 2
         else:
@@ -73,7 +93,7 @@ class StatusBuilder:
             for message in self.__warning:
                 print('WARNING: ' + message)
             if not all_outputs:
-                sys.exit(1)
+                self.__exit(1)
             elif None == exit_code:
                 exit_code = 1
         else:
@@ -83,7 +103,7 @@ class StatusBuilder:
             for message in self.__unknown:
                 print('UNKNOWN: ' + message)
             if not all_outputs:
-                sys.exit(3)
+                self.__exit(3)
             elif None == exit_code:
                 exit_code = 3
         else:
@@ -93,13 +113,13 @@ class StatusBuilder:
             for message in self.__success:
                 print('SUCCESS: ' + message)
             if not all_outputs:
-                sys.exit(0)
+                self.__exit(0)
             elif None == exit_code:
                 exit_code = 0
 
         if None != exit_code:
-            sys.exit(exit_code)
+            self.__exit(exit_code)
         self.__logger.debug('No success messages found, exit with unknown')
 
         print('UNKNOWN: No status message found')
-        sys.exit(3)
+        self.__exit(3)
